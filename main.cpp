50fa4de7fadff64eb6266bfb0b7b8b737c189222
ACSoverride <mattinhvt@gmail.com>
2022-09-21 01:57:45 -0400

    project currently working except for input validation with non number characters

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <cmath>
+#include <ios>
 using namespace std;
 int main() {
     //setting up variables for input
     double startCall;
     double timeCall;
     double fractional;
+    //variable for the validation loop
     bool looper = true;
     //input validation loop
     while (looper == true) {
@@ -13,9 +15,11 @@ int main() {
         cout << "\nPlease enter the when you started the call in HH.MM format";
         cin >> startCall;
         fractional = startCall - static_cast<int>(startCall);
+        //checking if the inputs are valid
         if (startCall <= 24.00 && startCall >= 00.00 && fractional <= 0.6) {
             cout << "\nPlease enter the total time of the call in minutes";
             cin >> timeCall;
+            //since the inputs are valid we do the calculations
             if (startCall >= 0 && startCall <= 06.59) {
                 cout << "\nThe cost of your call was $" << 0.12 * timeCall;
                 looper = false;
@@ -31,6 +35,7 @@ int main() {
                 looper = false;
             }
         }
+        //here since the input is not valid we tell the user what to do again and loop back to the start so they can try again
         else {
             cout << "\nSorry you did not enter a valid start time. Please put the start time in millitary time in the format HH.MM. Please press enter to continue.";
         }
