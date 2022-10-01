cd32bcb593906cd3ceca9f47d16d3cb5fb4ac54a
ACSoverride <mattinhvt@gmail.com>
2022-10-01 16:24:55 -0400

    finished project

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 #include <cmath>
 #include <ios>
+//Class : CSI 140-01
+// Assignment : PA x or Lab x
+// Date Assigned : 9/19/2022
+// Due Date : Due - 10/2/2022 11:59 pm
+// Description :
+//A program that calculates long distance call prices to mexico
+// Certification of Authenticity :
+//I certify that this is entirely my own work, except where I have given fully documented
+// references to the work of others.I understand the definition and
+// consequences of plagiarism and acknowledge that the assessor of this assignment
+// may, for the purpose of assessing this assignment :
+// -Reproduce this assignment and provide a copy to another member of
+// academic staff; and / or
+// - Communicate a copy of this assignment to a plagiarism checking service
+// (which may then retain a copy of this assignment on its database for
+// the purpose of future plagiarism checking)
 using namespace std;
 int main() {
     //setting up variables for input
